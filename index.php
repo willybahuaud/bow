046e97c03c17935b42d1295a174d842bc373daf5
Willy Bahuaud <w.bahuaud@gmail.com>
2013-06-19 22:02:25 +0200

    new index

--- index.php
+++ index.php
@@ -12,20 +12,29 @@ $router->map( 'GET|POST', '/flux/subscribe/', 'flux#add', 'flux_add' );
 $router->map( 'GET|POST', '/flux/import/', 'flux#import', 'flux_import' );
 $match = $router->match();
 
-if( 'home' == $match['name'] )
-    require('templates/home.php');
 
-if( 'flux_add' == $match['name'] )
-    require('templates/add_flux.php');
+$target     = explode( '#', $match[ 'target' ] );
+$controller = $target[0];
+$action     = $target[1];
+$params     = $match[ 'params' ];
 
-if( 'user_create' == $match['name'] )
-    require('templates/new_user.php');
+require_once('controller/' . $controller. '.php');
+$controller = new $controller();
+$controller->$action( $params );
+// if( 'home' == $match['name'] )
+//     require('templates/home.php');
 
-if( 'user_delete' == $match['name'] )
-    require('templates/delete_account.php');
+// if( 'flux_add' == $match['name'] )
+//     require('templates/add_flux.php');
 
-if( 'flux_import' == $match['name'] )
-    require('templates/import_flux.php');
+// if( 'user_create' == $match['name'] )
+//     require('templates/new_user.php');
+
+// if( 'user_delete' == $match['name'] )
+//     require('templates/delete_account.php');
+
+// if( 'flux_import' == $match['name'] )
+//     require('templates/import_flux.php');
 
 ?>
 
