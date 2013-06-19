824eee10de7c76e682db17ccf235b828e86b6bc6
be2ny <petit.benjamin13@gmail.com>
2013-06-19 22:05:45 +0200

    controllers et models
    papi papi !!

---
+++ controller/controller.flux.php
@@ -0,0 +1,31 @@
+<?php
+
+// ici on met les fichiers require
+
+
+public function indexAction(){
+
+
+}
+
+public function createAction(){
+
+// en gros la tu balmances ton $f->new flux();
+	   return $response;
+}
+
+
+public function readAction(){
+
+// en gros la tu balmances ton $f->flux();
+	   return $response;
+}
+
+
+public function deleteAction(){
+
+
+	   return $response;
+}
+
+?>
---
+++ controller/controller.users.php
@@ -0,0 +1,30 @@
+<?php
+
+// ici on met les fichiers require
+
+
+public function indexAction(){
+
+
+ return $response;
+}
+
+public function createAction(){
+
+ return $response;
+}
+
+
+public function readAction(){
+
+ return $response;
+}
+
+
+public function deleteAction(){
+
+ return $response;
+}
+
+
+?>
